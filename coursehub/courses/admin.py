935247447968e8295ba6b99a2bc045fdcb9a88de
radek <rajkonkret660@gmail.com>
2026-08-03 14:33:26 +0200

    [1 26] update app courses

--- coursehub/courses/admin.py
+++ coursehub/courses/admin.py
@@ -3,5 +3,21 @@ from .models import Course, Trainer
 
 # Register your models here.
 
-admin.site.register(Course)
+# admin.site.register(Course) # komentujemy po dodaniu dekoratora
 admin.site.register(Trainer)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "trainer",
+        "price",
+        "start_date",
+        "active"
+    )
+
+    list_filter = ("active", "start_date", "trainer")
+    search_fields = ("title", "description", "trainer__last_name")
+
+    list_select_related = ("trainer",)  # ominiecie problemu N + 1
