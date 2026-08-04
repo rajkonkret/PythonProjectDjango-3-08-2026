f79dd72b5f3466dd0baf2fc1b1fffd68c42a2612
radek <rajkonkret660@gmail.com>
2026-08-04 15:57:18 +0200

    [2 47] update app courses add Enroll

--- coursehub/config/settings.py
+++ coursehub/config/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+# pip install python-dotenv
 
-
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-62ugn!-a%l0996ws%&5==dddz^l&h_13@fy8%^ej&&z%w#p^e%'
-
+# SECRET_KEY = 'django-insecure-62ugn!-a%l0996ws%&5==dddz^l&h_13@fy8%^ej&&z%w#p^e%'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # DEBUG = False
