09f874b2841f3da9c566f5599f572e5bd90a93ba
radek <rajkonkret660@gmail.com>
2026-08-04 15:27:07 +0200

    [2 43] update app courses add Enroll

--- coursehub/courses/urls.py
+++ coursehub/courses/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("courses/<int:pk>/", views.course_detail, name="course_detail"),
     path("courses/new/", views.course_create, name="course_create"),
     path("courses/<int:pk>/enroll", views.enroll_in_course, name="enroll_in_course"),
+    path("my-courses/", views.my_courses, name="my_courses"),
 ]
 # ctrl alt l - formatowanie wg PEP8
