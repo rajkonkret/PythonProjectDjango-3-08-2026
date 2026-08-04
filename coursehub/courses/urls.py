16cd96300b251c1ad18887dd417b25e70c1fb031
radek <rajkonkret660@gmail.com>
2026-08-04 12:09:06 +0200

    [2 19] update app courses add __str__

--- coursehub/courses/urls.py
+++ coursehub/courses/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("courses/", views.course_list, name="course_list"),
-    path("courses/<int:pk>/", views.course_detail, name="course_detail")
+    path("courses/<int:pk>/", views.course_detail, name="course_detail"),
+    path("courses/new/", views.course_create, name="course_create")
 ]
 # ctrl alt l - formatowanie wg PEP8
