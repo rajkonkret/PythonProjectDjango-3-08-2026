fc16e12ad0932226ea537185bc887a19ed2e0b2c
radek <rajkonkret660@gmail.com>
2026-08-04 15:17:11 +0200

    [2 41] update app courses add Enroll

--- coursehub/courses/views.py
+++ coursehub/courses/views.py
@@ -86,6 +86,6 @@ def enroll_in_course(request, pk):
                 request,
                 "Jestes juz zapisany na ten kurs."
             )
-        redirect(course)  # dziąl dzieki get_absolute_url
+        redirect(course)  # działa dzięki get_absolute_url
 
     return redirect("courses:course_list")
